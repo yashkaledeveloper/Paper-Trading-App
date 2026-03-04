e19201cbaaca6ce29fbbbe207a25bc36b9851d0c
Yash <yashprogrammers@gmail.com>
2026-03-04 15:54:41 +0530

    Fresh init after unrecoverable git corruption

---
+++ backend/Routes/AllRoute.js
@@ -0,0 +1,41 @@
+const { getWallet } = require("../Controllers/WalletController");
+const { buyStock, sellStock, getUserOrders } = require("../Controllers/OrdersController");
+const { getWatchlist, addToWatchlist, removeFromWatchlist, currentPrice } = require("../Controllers/WatchListController");
+const { getHoldings } = require("../Controllers/HoldingController")
+
+const { userVerification } = require("../Middlewares/AuthMiddleware");
+const { StockModel } = require("../model/StockModel");
+
+const router = require("express").Router();
+
+router.post("/eachStockInfo", async (req, res) => {
+    let {_id} = req.body;
+    let data = await StockModel.findOne({_id})
+    res.json(data);
+})
+
+// allstocks
+router.get("/allstocks", async (req, res) => {
+    let data = await StockModel.find({});
+    res.json(data);
+})
+
+router.post("/realprice", userVerification, currentPrice)
+
+// user allholdings
+router.get("/allhodings", userVerification, getHoldings);
+
+// watchlist
+router.get("/getwatchlist", userVerification, getWatchlist);
+router.post("/addwatchlist", userVerification, addToWatchlist);
+router.post("/delwatchlist", userVerification, removeFromWatchlist);
+
+// user orders
+router.post("/buy", userVerification, buyStock);
+router.post("/sell", userVerification, sellStock);
+router.get("/allorders", userVerification, getUserOrders);
+
+// user wallet
+router.get("/getwallet", userVerification, getWallet);
+
+module.exports = router;
